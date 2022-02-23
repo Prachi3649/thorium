2f2e0fbbce292745b4f32ff8c6656baca3203f88
Prachi <prachisingh281599@gmail.com>
2022-02-23 23:57:25 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,99 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+//1
+
+router.get("/movies", function (req, res) {
+    const arrayOfMovies = [
+      "Batman",
+      "Inception",
+      "Harry Potter",
+      "Lord of The Rings",
+      "Tron",
+    ];
+    res.send(arrayOfMovies);
+  });
+
+  //2 or 3
+  
+  router.get("/movies/:index", function (req, res) {
+    const arrayOfMovies = [
+      "Batman",
+      "Inception",
+      "Harry Potter",
+      "Lord of The Rings",
+      "Tron",
+    ];
+    let c = req.params.index;
+    if (arrayOfMovies.length - 1 < c) {
+      res.send("Error! Index out of bounds.");
+    } else {
+      res.send(arrayOfMovies[c]);
+    }
+  });
+
+  //4
+  
+  router.get("/films", function (req, res) {
+    const arrayOfFilms = [
+      {
+        id: 1,
+        name: "Dune",
+      },
+      {
+        id: 2,
+        name: "Wall Street",
+      },
+      {
+        id: 3,
+        name: "Big Short",
+      },
+      {
+        id: 4,
+        name: "Pirates of Carribean",
+      },
+    ];
+    res.send(arrayOfFilms);
+  });
+
+  //5
+  
+  router.get("/films/:filmId", function (req, res) {
+    const arrayOfFilms = [
+      {
+        id: 1,
+        name: "Dune",
+      },
+      {
+        id: 2,
+        name: "Wall Street",
+      },
+      {
+        id: 3,
+        name: "Big Short",
+      },
+      {
+        id: 4,
+        name: "Pirates of Carribean",
+      },
+    ];
+  
+    let value = req.params.filmId;
+    let found = false;
+    for (let i = 0; i < arrayOfFilms.length; i++) {
+      if (arrayOfFilms[i].id == value) {
+        found = true;
+        res.send(arrayOfFilms[i]);
+        break;
+      }
+    }
+    if (found == false) {
+      res.send("No movie with given index exists!");
+    }
+  });
+  
+  module.exports = router;
 
 
 
-module.exports = router;
+
