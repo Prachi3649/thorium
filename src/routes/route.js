4d767fd7a1f12681ef82502b84fbf7462a3bbeb7
Prachi <prachisingh281599@gmail.com>
2022-03-01 10:34:38 +0530

    assignment_mongo

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
+const bookModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+router.post("/book",async function(req, res){
+ const data =req.body
+ const saveData = await bookModel.create(data)
+ res.send({message:saveData})
+})
+
+router.get("/booklist",async function(req, res){
+    const saveData = await bookModel.find()
+    res.send({message:saveData})
+   })
 
 router.post("/createUser", UserController.createUser  )
 
