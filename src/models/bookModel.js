49115821e1dfaef92e17014f3d841b7c732fabc1
Prachi <prachisingh281599@gmail.com>
2022-03-05 00:19:50 +0530

    assignment_populate

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,23 @@
 const mongoose = require('mongoose');
+const { required } = require('nodemon/lib/config');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
         type: ObjectId,
-        ref: "Author"
+        required:true,
+        ref: "newAuthor"
     },
     price: Number,
-    ratings: Number
-
+    ratings: Number,
+    publisher:{
+        type:ObjectId,
+        ref:"newPublisher",
+        required:true
+    }
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('newBook', bookSchema)
