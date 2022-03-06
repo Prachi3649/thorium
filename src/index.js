6b35358e18799434c175ffd7d2f59ae07fef4ee8
Prachi <prachisingh281599@gmail.com>
2022-03-06 21:42:38 +0530

    solution/Middleware-intro

--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req,res,next){
+    let todayDetaile=new Date()
+    let dayA=todayDetaile.getDate()
+    let monthB=todayDetaile.getMonth()
+    let yearC=todayDetaile.getFullYear()
+    let time=todayDetaile.toUTCString().substring(17,25)
+    const datestring=yearC+"-"+(monthB+1)+"-"+dayA+" "+time
+    let ipADD=req.socket.remoteAddress //connection object also use but this object can be Deprecated
+    console.log(datestring,ipADD,req.path)
+    next();
+    
+    })
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Prachi3649--DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
